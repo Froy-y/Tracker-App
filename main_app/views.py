24568fbb4e4234b4a8686742c86ca8801a6a6a86
Froylan <froy.push@gmail.com>
2021-09-17 00:28:27 -0500

    Commit 1 - Created Home url, view, and html with base.html too

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+#home
+def home(request):
+    return render(request, 'home.html')
+
+#index
+
+#detail (show)
+
+#create
+
+#update
+
+#delete
+
+#one to one
+
+#many to many -classes-
+#index
+
+#detail (show)
+
+#create
+
+#associations
+#assoc
+
+#unassoc
+
+#photo -aws-
+
+#signup -auth-
